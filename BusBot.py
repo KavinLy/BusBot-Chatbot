77d4989cdc49c56d7b7463a8e5717980203da414
Kavin Ly <57770647+KavinLy@users.noreply.github.com>
2022-07-27 17:00:20 +0100

    Removed personal API key

--- BusBot.py
+++ BusBot.py
@@ -4,139 +4,139 @@ import json
 def askbot(bus_stop):
 
     if bus_stop == "CU2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053801/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053801/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CU1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000006201/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000006201/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "MP1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43009012501/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43009012501/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "SJ1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43009012601/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43009012601/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BY4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002103/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002103/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "WR1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008201/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008201/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "WR3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008203/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008203/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BY1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002101/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002101/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "LP1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000007201/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000007201/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "ES1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000007102/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000007102/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CU5":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053502/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053502/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "FX1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000006301/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000006301/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "TS3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001003/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001003/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BS6":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001202/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001202/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CS3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003203/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003203/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "VR2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001006502/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001006502/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "GR2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008302/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008302/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BS5":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001203/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001203/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CS1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003201/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003201/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CR4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009104/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009104/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "VR3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001006501/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001006501/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CS4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003206/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003206/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BS3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003102/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003102/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "UL3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004104/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004104/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CR2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009102/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009102/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CS2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003202/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000003202/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CU4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053702/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001053702/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "TS5":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001054401/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001054401/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "ER4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008105/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008105/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "ER1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008101/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008101/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "GR1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008301/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000008301/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CU3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001054201/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001054201/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BS4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001204/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001204/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "UL1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004106/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004106/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "UL4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004101/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000004101/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "HS1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001101/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001101/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "TS4":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001004/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001004/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "TS1":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001001/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001001/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BY5":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002104/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002104/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "HS3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001025501/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43001025501/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "TS2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001002/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001002/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "HS2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001102/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000001102/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "CR3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009103/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000009103/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BY3":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002105/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002105/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     elif bus_stop == "BY2":
-        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002102/live.json?app_id=cac11064&app_key=dde28481a2aadc583d6425adc379637e&group=route&nextbuses=yes")
+        url = urlopen("https://transportapi.com/v3/uk/bus/stop/43000002102/live.json?app_id=cac11064&app_key=API_KEY_HERE&group=route&nextbuses=yes")
         departures(url, bus_stop)
     else:
         print("That bus stop is not on my list! Try another")
